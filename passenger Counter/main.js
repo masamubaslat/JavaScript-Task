3575771c3979a54bf41ef9bfd2d5a3520d8bdf01
masamubaslat <masa.mubaslat111#gmail.com>
2023-01-27 15:29:44 +0200

    second commit

--- passenger Counter/main.js
+++ passenger Counter/main.js
@@ -1,10 +1,14 @@
 let counter = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el');
 let count =0 ;
 function increment(){
     count+=1;
-    counter.innerHTML = count;
+    counter.textContent = count;
 }
 
 function save(){
-    alert('hi')
+    let countStr = count + ' - ';
+    saveEl.textContent += countStr;
+    counter.textContent = 0;
+    count =0 ;
 }
